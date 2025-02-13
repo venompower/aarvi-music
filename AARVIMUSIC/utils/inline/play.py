2850f0bc6fbb18881f876fc8d6d3e5fa0bc04f74
VENOM <venommusic91@gmail.com>
2025-02-13 21:27:27 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/utils/inline/play.py
+++ AARVIMUSIC/utils/inline/play.py
@@ -1,9 +1,9 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-from AARVIMUSICMUSIC import app
+from AARVIMUSIC import app
 import config
-from AARVIMUSICMUSIC.utils.formatters import time_to_seconds
+from AARVIMUSIC.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
