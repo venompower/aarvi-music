9b7d28e70217a26e175194e1e9c33bb259f04b89
VENOM <venommusic91@gmail.com>
2025-02-13 20:07:43 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/__init__.py
+++ AARVIMUSIC/__init__.py
@@ -1,1 +1,27 @@
+from AARVIMUSIC.core.bot import AARVI
+from AARVIMUSIC.core.dir import dirr
+from AARVIMUSIC.core.git import git
+from AARVIMUSIC.core.userbot import Userbot
+from AARVIMUSIC.misc import dbb, heroku
+from pyrogram import Client
+from SafoneAPI import SafoneAPI
+from .logging import LOGGER
 
+dirr()
+git()
+dbb()
+heroku()
+
+app = AARVI()
+api = SafoneAPI()
+userbot = Userbot()
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
---
+++ AARVIMUSIC/__main__.py
@@ -0,0 +1,59 @@
+
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from AARVIMUSIC import LOGGER, app, userbot
+from AARVIMUSIC.core.call import AARVI
+from AARVIMUSIC.misc import sudo
+from AARVIMUSIC.plugins import ALL_MODULES
+from AARVIMUSIC.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+from AARVIMUSIC.plugins.tools.clone import restart_bots
+
+
+async def init():
+    if not config.STRING1:
+        LOGGER(__name__).error("String Session not filled, please provide a valid session.")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("AARVIMUSIC.plugins" + all_module)
+    LOGGER("AARVIMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
+    await userbot.start()
+    await AARVI.start()
+    try:
+        await AARVI.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("AARVIMUSIC").error(
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗠𝗨𝗦𝗜𝗖 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
+        )
+        exit()
+    except:
+        pass
+    await AARVI.decorators()
+    await restart_bots()
+    LOGGER("AARVIMUSIC").info(
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗼𝗼𝗻𝗟𝗶𝗴𝗵𝘁☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("AARVIMUSIC").info("𝗦𝗧𝗢𝗣 𝗠𝗼𝗼𝗻𝗟𝗶𝗴𝗵𝘁 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
