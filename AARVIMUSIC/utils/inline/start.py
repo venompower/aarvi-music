b51ed2136609444d68d3ed5a3268627729909caf
VENOM <venommusic91@gmail.com>
2025-02-13 21:42:15 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/utils/inline/start.py
+++ AARVIMUSIC/utils/inline/start.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, Message
 import config
 import asyncio
-from AARVIMUSICMUSIC import app
+from AARVIMUSIC import app
 
 
 # Start panel for inline buttons
