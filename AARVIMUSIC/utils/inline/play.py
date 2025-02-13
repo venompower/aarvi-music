3ea51a6e9cbd3ba3471f59afaedea59eaa0c1015
VENOM <venommusic91@gmail.com>
2025-02-13 23:05:03 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/utils/inline/play.py
+++ AARVIMUSIC/utils/inline/play.py
@@ -79,8 +79,14 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],    
-          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+        ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            )
+        ],        
+        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
