09e1feaf3b4145b2c92f479c9c71e9d29ae35f40
VENOM <venommusic91@gmail.com>
2025-02-15 13:27:07 +0530

    ᴀᴀʀᴠɪ ᴍᴜsɪᴄ

--- AARVIMUSIC/utils/stream/stream.py
+++ AARVIMUSIC/utils/stream/stream.py
@@ -1,20 +1,68 @@
 import os
 from random import randint
 from typing import Union
+import random
+import string
+import asyncio
+from pyrogram import client, filters
+from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
+from pytgcalls.exceptions import NoActiveGroupCall
+from AARVIMUSIC.utils.database import get_assistant
+import config
+from AARVIMUSIC import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from AARVIMUSIC.core.call import BABY
+from AARVIMUSIC.misc import SUDOERS
+from AARVIMUSIC.utils import seconds_to_min, time_to_seconds
+from AARVIMUSIC.utils.channelplay import get_channeplayCB
+from AARVIMUSIC.utils.decorators.language import languageCB
+from AARVIMUSIC.utils.decorators.play import PlayWrapper
+from AARVIMUSIC.utils.formatters import formats
+from AARVIMUSIC.utils.inline import (
+    botplaylist_markup,
+    livestream_markup,
+    playlist_markup,
+    slider_markup,
+    track_markup,
+)
+from AARVIMUSIC.utils.database import (
+    add_served_chat,
+    add_served_user,
+    blacklisted_chats,
+    get_lang,
+    is_banned_user,
+    is_on_off,
+)
+from AARVIMUSIC.utils.logger import play_logs
+from config import BANNED_USERS, lyrical
+from time import time
+from AARVIMUSIC.utils.extraction import extract_user
+
+# Define a dictionary to track the last message timestamp for each user
+user_last_message_time = {}
+user_command_count = {}
+# Define the threshold for command spamming (e.g., 20 commands within 60 seconds)
+SPAM_THRESHOLD = 2
+SPAM_WINDOW_SECONDS = 5
+
 
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from AARVIMUSIC import Carbon, YouTube, app
-from AARVIMUSIC.core.call import AARVI
+from AARVIMUSIC.core.call import BABY
 from AARVIMUSIC.misc import db
 from AARVIMUSIC.utils.database import add_active_video_chat, is_active_chat
 from AARVIMUSIC.utils.exceptions import AssistantErr
-from AARVIMUSIC.utils.inline import aq_markup, close_markup, stream_markup
+from AARVIMUSIC.utils.inline import (
+    aq_markup,
+    close_markup,
+    stream_markup,
+    stream_markup2,
+    panel_markup_4,
+)
+from AARVIMUSIC.utils.pastebin import PROBin
 from AARVIMUSIC.utils.stream.queue import put_queue, put_queue_index
-from AARVIMUSIC.utils.pastebin import AARVIBin
 from youtubesearchpython.__future__ import VideosSearch
-from AARVIMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -33,7 +81,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await AARVI.force_stop_stream(chat_id)
+        await BABY.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,8 +127,9 @@ async def stream(
                         vidid, mystic, video=status, videoid=True
                     )
                 except:
-                    raise AssistantErr(_["play_14"])
-                await AARVI.join_call(
+
+                    await mystic.edit_text(_["play_3"])
+                await BABY.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -100,24 +149,25 @@ async def stream(
                     forceplay=forceplay,
                 )
                 img = await get_thumb(vidid)
-                button = stream_markup(_, chat_id)
+                button = stream_markup(_, vidid, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
                     photo=img,
                     caption=_["stream_1"].format(
                         f"https://t.me/{app.username}?start=info_{vidid}",
-                        title[:23],
+                        title[:18],
                         duration_min,
                         user_name,
                     ),
                     reply_markup=InlineKeyboardMarkup(button),
                 )
+
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "stream"
         if count == 0:
             return
         else:
-            link = await AARVIBin(msg)
+            link = await PROBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -125,9 +175,10 @@ async def stream(
                 car = msg
             carbon = await Carbon.generate(car, randint(100, 10000000))
             upl = close_markup(_)
-            return await app.send_text(
+            return await app.send_photo(
                 original_chat_id,
-                text=_["play_21"].format(position, link),
+                photo=carbon,
+                caption=_["play_21"].format(position, link),
                 reply_markup=upl,
             )
     elif streamtype == "youtube":
@@ -142,7 +193,8 @@ async def stream(
                 vidid, mystic, videoid=True, video=status
             )
         except:
-            raise AssistantErr(_["play_14"])
+
+            await mystic.edit_text(_["play_3"])
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -155,17 +207,21 @@ async def stream(
                 user_id,
                 "video" if video else "audio",
             )
+            img = await get_thumb(vidid)
             position = len(db.get(chat_id)) - 1
             button = aq_markup(_, chat_id)
-            await app.send_message(
+            await app.send_photo(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                photo=img,
+                caption=_["queue_4"].format(
+                    position, title[:18], duration_min, user_name
+                ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await AARVI.join_call(
+            await BABY.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -185,18 +241,19 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = stream_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
                 caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{vidid}",
-                    title[:23],
+                    title[:18],
                     duration_min,
                     user_name,
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
+
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
     elif streamtype == "soundcloud":
@@ -219,13 +276,13 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await AARVI.join_call(chat_id, original_chat_id, file_path, video=None)
+            await BABY.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -238,7 +295,7 @@ async def stream(
                 "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
@@ -271,13 +328,13 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
             if not forceplay:
                 db[chat_id] = []
-            await AARVI.join_call(chat_id, original_chat_id, file_path, video=status)
+            await BABY.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -292,7 +349,7 @@ async def stream(
             )
             if video:
                 await add_active_video_chat(chat_id)
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=config.TELEGRAM_VIDEO_URL if video else config.TELEGRAM_AUDIO_URL,
@@ -324,7 +381,7 @@ async def stream(
             button = aq_markup(_, chat_id)
             await app.send_message(
                 chat_id=original_chat_id,
-                text=_["queue_4"].format(position, title[:27], duration_min, user_name),
+                text=_["queue_4"].format(position, title[:18], duration_min, user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
         else:
@@ -333,7 +390,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await AARVI.join_call(
+            await BABY.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -353,11 +410,11 @@ async def stream(
                 forceplay=forceplay,
             )
             img = await get_thumb(vidid)
-            button = stream_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
                 photo=img,
-                text=_["stream_1"].format(
+                caption=_["stream_1"].format(
                     f"https://t.me/{app.username}?start=info_{vidid}",
                     title[:23],
                     duration_min,
@@ -391,7 +448,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await AARVI.join_call(
+            await BABY.join_call(
                 chat_id,
                 original_chat_id,
                 link,
@@ -408,23 +465,38 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            button = stream_markup(_, chat_id)
-            run = await app.send_message(
+            button = stream_markup2(_, chat_id)
+            run = await app.send_photo(
                 original_chat_id,
-                text=_["stream_2"].format(user_name),
+                photo=config.STREAM_IMG_URL,
+                caption=_["stream_2"].format(user_name),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
             await mystic.delete()
 
-# async def get_thumb(videoid):
-#     try:
-#         # Search for the video using video ID
-#         query = f"https://www.youtube.com/watch?v={videoid}"
-#         results = VideosSearch(query, limit=1)
-#         for result in (await results.next())["result"]:
-#             thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-#         return thumbnail
-#     except Exception as e:
-#         return config.YOUTUBE_IMG_URL
+
+# Function to get thumbnail by video ID
+async def get_thumb(videoid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
+
+
+async def get_thumb(vidid):
+    try:
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
+    except Exception as e:
+        return config.YOUTUBE_IMG_URL
